1d526a4a101c3abe6b3e19118633eb3dd8d298a1
AdansDev <adansprogramador@gmail.com>
2023-07-06 18:26:47 -0400

    ajustes de código

--- src/components/IncrementDecrement.tsx
+++ src/components/IncrementDecrement.tsx
@@ -1,21 +1,52 @@
-import style from "./IncrementDecrement.module.css"
+import { useState } from "react";
+import style from "./IncrementDecrement.module.css";
+
 function IncrementDecrement() {
+  let result = 0
+  function handleDecrementButton() {
+    result--
+  }
+  // estado - state
+  const [count, setCount] = useState(0);
+
+  // function handleDecrementcount() {
+  //   setCount(count - 1)
+  // }
+  function handleincrementcount() {
+    setCount(count + 1)
+  }
+
   return (
     <div className={style.container}>
-      <div>
-        <h2>Incrementing and Decrementing a number </h2>
-        <div>
-          <span>Usando variável</span>
-          <button>
-            <span> - </span>
-          </button>
+      <h2>Incrementing and Decrementing a number </h2>
+      <div className={style.counter}>
+        <span>Usando variável</span>
+        <button onClick={handleDecrementButton}>
+          <span> - </span>
+        </button>
 
-          <span>10</span>
+        <span className={style.value}>{result}</span>
 
-          <button>
-            <span> + </span>
-          </button>
-        </div>
+        <button onClick={handleDecrementButton}>
+          <span> + </span>
+        </button>
+      </div>
+
+
+      <hr />
+
+ 
+      <div className={style.counter}>
+        <span>Usando estado</span>
+        <button onClick={()=> setCount(count - 1)}>
+          <span> - </span>
+        </button>
+
+        <span className={style.value}>{count}</span>
+
+        <button onClick={handleincrementcount}>
+          <span> + </span>
+        </button>
       </div>
     </div>
   );
